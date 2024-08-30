5c961805ab67248204cefe722f9a02fc553ba51d
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-08-30 07:28:37 +0300

    cleanup

--- babyagi/main.py
+++ babyagi/main.py
@@ -2,16 +2,6 @@ from hyperon import *
 from hyperon.ext import register_atoms
 from .agents import *
 
-# @register_atoms(pass_metta=True)
-# def babyagi_atoms(metta):
-#     gptAtom = OperationAtom('gpt', lambda *args: prompt_agent(metta, *args), [AtomType.ATOM, "Expression"], unwrap=False)
-#     cnnAtom = OperationAtom('cnn', lambda *args: detect_skin_cancer(metta, *args), unwrap=False)
-
-#     return {
-#         r"gpt": gptAtom,
-#         r"cnn": cnnAtom
-#     }
-
 @register_atoms(pass_metta=True)
 def babyagi_atoms(metta):
     sentimentAnalyzerAtom = OperationAtom('sntmnt', lambda *args: analyze_sentiment(metta, *args), unwrap=False)
